4d5cac8c116cce3c81dd0fc4d3d61efe030eaf8d
Leo <nikolenko@inbox.lv>
2016-04-18 21:28:48 +0300

    Added menu and search button

--- app/src/main/java/com/leo/remindme/MainActivity.java
+++ app/src/main/java/com/leo/remindme/MainActivity.java
@@ -2,15 +2,31 @@ package com.leo.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
-/**
- * Created by Leo on 2016.04.18..
- */
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    public void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
